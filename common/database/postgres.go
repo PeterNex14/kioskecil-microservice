9155a206087ca3ac6ccba53e86885343f3fc08da
PeterNex14 <peternex42@gmail.com>
2026-04-17 01:58:07 +0800

    fixing database connection problem

--- common/database/postgres.go
+++ common/database/postgres.go
@@ -3,30 +3,32 @@ package database
 import (
 	"database/sql"
 	"fmt"
-	"os"
+
+	"github.com/PeterNex14/kioskecil-microservice/common/config"
 )
 
-
 func InitDB() (*sql.DB, error) {
+	// Use config.GetEnv to provide default values if environment variables are missing
+	host := config.GetEnv("DB_HOST", "localhost")
+	user := config.GetEnv("USER_DB_USER", "postgres")
+	password := config.GetEnv("USER_DB_PASSWORD", "password")
+	dbname := config.GetEnv("USER_DB_NAME", "postgres")
+	port := config.GetEnv("DB_PORT", "5432")
 
+	// Construct DSN string safely
 	dsn := fmt.Sprintf(
 		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Jakarta",
-		os.Getenv("DB_HOST"),
-		os.Getenv("DB_USER"),
-		os.Getenv("DB_PASSWORD"),
-		os.Getenv("DB_NAME"),
-		os.Getenv("DB_PORT"),
+		host, user, password, dbname, port,
 	)
 
 	db, err := sql.Open("postgres", dsn)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("error opening database: %v", err)
 	}
 
 	if err := db.Ping(); err != nil {
-		return nil, err
+		return nil, fmt.Errorf("error connecting to database [%s]: %v", dbname, err)
 	}
-	
 
 	return db, nil
 }
